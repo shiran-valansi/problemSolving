7cdb30d1af06d04ce07860c47f8eee6342a979cc
Shiran <shiran.valansi@gmail.com>
2021-04-30 16:52:51 -0700

    keys in rooms bfs

---
+++ keysInRooms.py
@@ -0,0 +1,34 @@
+def canVisitAllRooms(rooms):
+    """
+    There are N rooms and you start in room 0.  
+    Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room. 
+    A key rooms[i][j] = v opens the room with number v.
+    All the rooms start locked (except for room 0). 
+    You can walk back and forth between rooms freely.
+
+    Return true if and only if you can enter every room.
+
+    >>> canVisitAllRooms([[1,3],[3,0,1],[2],[0]])
+    False
+
+    >>> canVisitAllRooms([[1],[2],[3],[]])
+    True
+
+    """
+    ##### BFS #####    
+    q = [0]
+    visited = {0}
+
+    while q:
+        curr = q.pop(0)
+        for child in rooms[curr]:
+            if child in visited:
+                continue
+            visited.add(child)
+            q.append(child)
+
+    return len(visited) == len(rooms)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
